caf182ff9a8e15dd11e02304cd3f56804e62fd54
Jose Carlos <josealarconchacon@gmail.com>
2020-12-11 12:40:36 -0500

    number of characters in a string

--- function.py
+++ function.py
@@ -1,1 +1,8 @@
+# input() will get user input in the console
+# Then print() will print the world Hello, and the user input
 print("Hello " + input("WWhat is you name?"))
+
+
+# prints the number of characters in a user's name
+# calculate the length of the string with  len()
+print( len( input("What is your name?  ") ) )
