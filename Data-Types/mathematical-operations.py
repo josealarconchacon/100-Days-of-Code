fac931ab2adca90f33909b3193da77737e270254
Jose Carlos <josealarconchacon@gmail.com>
2020-12-28 14:55:00 -0500

    Mathematical Operations in Python

---
+++ Data-Types/mathematical-operations.py
@@ -0,0 +1,25 @@
+
+# addition
+print(3 + 7)
+
+# subtraction
+print(5 - 3)
+
+# multiplication
+print(3 * 3)
+
+# division
+print(10 / 2)
+
+# exponentiation
+print(3 ** 4)
+
+
+
+# PEMDAS
+# ()
+# **
+# *
+# /
+# + 
+# =
