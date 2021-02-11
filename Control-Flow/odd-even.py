8307336201616d7dd31c9a7c7d1bf9b67799145e
Jose Carlos <josealarconchacon@gmail.com>
2021-02-11 13:03:00 -0500

    check even or odd

---
+++ Control-Flow/odd-even.py
@@ -0,0 +1,10 @@
+#  7 % 3
+#  3 + 3 + 1
+
+number = int(input("Which number do you want to check? "))
+if number % 3 == 0:
+    print("This is a odd number")
+else:
+    print("This is a even number")
+
+
