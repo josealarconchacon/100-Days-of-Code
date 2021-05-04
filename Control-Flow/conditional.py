c8e75bfc640a38e38f40a02acb5863600c713cf2
Jose Carlos <josealarconchacon@gmail.com>
2021-05-04 13:34:31 -0400

    update repo

--- Control-Flow/conditional.py
+++ Control-Flow/conditional.py
@@ -1,7 +1,7 @@
 print("Welcome to the City")
-height = int(input("What is your height?"))
+height = int(input("What is your height in cm ?"))
 
 if height > 100:
-    print("You are allowed to enter the club")
+    print("You are allowed to enter the club ")
 else:
     print("Sorry, you are too short")
