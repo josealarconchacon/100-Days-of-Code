f0b99fcbade698fbe7f27463f904de738344f460
Jose Carlos <josealarconchacon@gmail.com>
2021-06-03 15:42:46 -0400

    string

---
+++ js/js_basics/string.js
@@ -0,0 +1,23 @@
+let email = "mail@gmail.com";
+console.log(email);
+
+// concatenation
+let first_name = "Tom";
+let last_name = "Perez";
+let full_name = first_name + ' '+last_name;
+console.log(full_name);
+
+// getting char
+console.log(full_name[1]);
+
+// string length
+console.log(full_name.length);
+
+// string methods
+console.log(full_name.toUpperCase());
+let lowerC = full_name.toLowerCase();
+console.log(lowerC);
+
+// find the index of the @
+let index = email.indexOf('@');
+console.log(index);
