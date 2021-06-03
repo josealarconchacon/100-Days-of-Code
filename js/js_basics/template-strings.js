87692e6a8d89f5e7c5f491516bf81e809789929b
Jose Carlos <josealarconchacon@gmail.com>
2021-06-03 15:58:27 -0400

    template string

---
+++ js/js_basics/template-strings.js
@@ -0,0 +1,7 @@
+const email = 'marionero@gmail.com';
+const name = 'Mario';
+const age = 98;
+
+// template string
+let result = 'The email is ${email} name ${name} age ${age}';
+console.log(result);
