6035c66d9a7b710a1df127b4f99ce58f21de2afe
Jose Carlos <josealarconchacon@gmail.com>
2021-06-04 13:24:33 -0400

    fucntion declaration and expression

---
+++ js/function_methods/func.js
@@ -0,0 +1,11 @@
+function hello() {
+        console.log('Hello');
+}
+
+// function expression
+const speak = function() {
+    console.log('I am having a good day');
+}
+// invoke the func
+hello();
+speak();
