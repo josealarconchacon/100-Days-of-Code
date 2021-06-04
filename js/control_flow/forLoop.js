2505f548ff1d71a6fd4827efe767006431d7b2e4
Jose Carlos <josealarconchacon@gmail.com>
2021-06-04 12:21:58 -0400

    for loop

---
+++ js/control_flow/forLoop.js
@@ -0,0 +1,12 @@
+// for loops
+for(let i = 0; i < 10; i++) {
+    console.log('loops',i);
+}
+console.log('loop finished!');
+// ---------------------------------------------------
+
+const student_name = ['Maria','Tom','Tara','Robin','Sam'];
+let i;
+for(i = 0; i < student_name.length; i++) {
+    console.log(student_name[i]);
+}
