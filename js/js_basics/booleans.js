729b31230d43aeb34fbea80d5aace2c32625e346
Jose Carlos <josealarconchacon@gmail.com>
2021-06-04 12:01:32 -0400

    comparation operators

---
+++ js/js_basics/booleans.js
@@ -0,0 +1,21 @@
+console.log(true, false, "true", "false");
+
+// methods can also return booleans
+let email = 'mario@gmail.com';
+let name = ['luis', 'mario', 'tomaz'];
+
+let result = email.includes('!');
+console.log(result); // false
+
+let result1 = name.includes('mario');
+console.log(result1); // true
+
+// comparation operators
+let age = 80;
+console.log(age == 70); 
+console.log(age != 70); 
+console.log(age < 70);
+console.log(age > 70);
+console.log(age <= 70);
+console.log(age >= 70);
+
---
+++ js/js_basics/null-underfined.js
@@ -0,0 +1,2 @@
+let number;
+console.log(number, number + 5, 'The number is ${number}');
