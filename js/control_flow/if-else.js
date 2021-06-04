a1941d65f703513af6daaa6dae5026a906df4ae7
Jose Carlos <josealarconchacon@gmail.com>
2021-06-04 12:52:31 -0400

    logical operators

--- js/control_flow/if-else.js
+++ js/control_flow/if-else.js
@@ -4,11 +4,11 @@ if(car.length > 4) {
 }
 // --------------------------------------------
 
-const password = "the@pass29927399";
+const password = "the@phre";
 
-if(password.length > 12) {
+if(password.length >= 12 && password.includes('@')) {
     console.log('Password is might strong');
-} else if(password.length >= 8) {
+} else if(password.length >= 8 || password.includes('@')) {
     console.log('Password is to long!!!!');
 } else {
     console.log('Password is not long!!!!');
