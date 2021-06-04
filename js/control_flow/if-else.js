61f650820be1d4477de08359bbeb06c0e6f41b70
Jose Carlos <josealarconchacon@gmail.com>
2021-06-04 12:48:16 -0400

    if else

---
+++ js/control_flow/if-else.js
@@ -0,0 +1,16 @@
+const car = ['Honda','Tesla','BMW','Toyota'];
+if(car.length > 4) {
+    console.log('To much cars');
+}
+// --------------------------------------------
+
+const password = "the@pass29927399";
+
+if(password.length > 12) {
+    console.log('Password is might strong');
+} else if(password.length >= 8) {
+    console.log('Password is to long!!!!');
+} else {
+    console.log('Password is not long!!!!');
+}
+
