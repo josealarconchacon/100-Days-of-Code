0d22f5c3784cafbe0de3dc5abc0ac36180858790
Jose Carlos <josealarconchacon@gmail.com>
2021-06-04 13:07:44 -0400

    switch

---
+++ js/control_flow/switch.js
@@ -0,0 +1,15 @@
+const student_grade = 'B';
+
+switch(student_grade) {
+    case 'A':
+        console.log("Student grad is A");
+        break;
+    case 'B':
+        console.log("Student grad is B");
+        break;
+    case 'C':
+        console.log("Student grad is C");
+        break;
+    default:
+        console.log("Invalid student grad");
+}
