c9a28bf5e6bae662c1d1f91d66774202e4f8af67
Jose Carlos <josealarconchacon@gmail.com>
2021-06-04 12:11:33 -0400

    type conversion

---
+++ js/js_basics/type-conversion.js
@@ -0,0 +1,9 @@
+let score_number = '200';
+// convert the score_number string into a number
+score_number = Number(score_number); 
+console.log(score_number + 10);
+console.log(typeof score_number);
+
+
+let convert = String(100);
+console.log(convert, typeof convert);
