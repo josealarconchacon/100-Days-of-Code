08eed3c7ebe36a66dab2c714830dd542aedc08fb
Jose Carlos <josealarconchacon@gmail.com>
2021-06-04 22:15:45 -0400

    randomisation

--- Randomisation/randomisation.py
+++ Randomisation/randomisation.py
@@ -5,3 +5,16 @@ if random_num == 1:
     print("Head")
 else:
     print("Tail")
+
+    # --------------------------------------
+
+names_string = input("write the desired names separated by comma: ")
+names = names_string.split(", ")
+# get the total number of item in the list
+num_item = len(names)
+
+pick_name = random.randint(0, num_item - 1)
+name_of_person = names[pick_name]
+print(name_of_person + " was selected")
+
+
