db01ea02008c64b8cfe583b7e738e8eec7e813e4
Jose Carlos <josealarconchacon@gmail.com>
2021-06-16 11:09:06 -0400

    new folder

---
+++ js/function_methods/arguments_parameters.js
@@ -0,0 +1,6 @@
+
+// function expression
+const speak = function(name, time) {
+    console.log(`${name} is having a good ${time}`);
+};
+speak('Tom', 'morning');
---
+++ js/function_methods/returning_value.js
@@ -0,0 +1,12 @@
+
+// Returing value
+const pi = 3.14;
+const calculate_area = function(radius) {
+    let area = pi * radius**2;
+    return area;
+};
+
+const area = calculate_area(5);
+console.log(area);
+
+
