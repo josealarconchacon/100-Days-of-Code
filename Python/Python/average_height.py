4ea1f6d83873469d1142e40a9088c538cae044b1
Jose Carlos <josealarconchacon@gmail.com>
2021-06-16 11:30:35 -0400

    get/Library/Frameworks/Python.framework/Versions/3.9/bin/python3 /Users/josealarconchacon/Documents/100-Days-of-Code/Python/Python/average_height.py

--- Python/Python/average_height.py
+++ Python/Python/average_height.py
@@ -1,2 +1,16 @@
 
+# calculates the average height from a List of heights
 heights = [180, 124, 165, 173, 189, 169, 146]
+
+total_height = 0
+
+# loop through the list of heights
+for height in heights:
+    # summ all heights
+    total_height += height
+print(total_height)
+
+number_of_height = 0
+for i in heights:
+    number_of_height += 1
+print(number_of_height)
