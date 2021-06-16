ba83d485cbd0b1b3d48e80a73b4ce1500e293a60
Jose Carlos <josealarconchacon@gmail.com>
2021-06-16 12:12:21 -0400

    update files

--- Python/Python/adding_even.py
+++
@@ -1,8 +0,0 @@
-
-# sum of all the even numbers from 1 to 100
-sum_of_even = 0
-
-for numbers in range(1,101):
-    if(numbers % 2 == 0):
-        sum_of_even += numbers
-print(f"Sum of all even numbers is: {sum_of_even}")
--- Python/loop/loop_range_func.py
+++
@@ -1,6 +0,0 @@
-# sum all number from 1 to 100
-# loop with tange
-total = 0
-for num in range(1, 101):
-    total += num
-print(total)
