81b5c0eeeba714907b6b0294c6235050f30a949c
Jose Carlos <josealarconchacon@gmail.com>
2021-06-17 10:54:52 -0400

    Creating and removing elements

--- js/dom/Event/main.js
+++ js/dom/Event/main.js
@@ -1,17 +1,24 @@
-// const button = document.querySelector('button');
-// button.addEventListener('click', () => {
-//     console.log('Button clicked');
-// });
+
+const ul = document.querySelector('ul');
+
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+    // DOM method to create an new html element
+   const li = document.createElement('li');
+   li.textContent = 'new item';
+//    ul.append(li);
+   ul.prepend(li);
+});
+
 
 const items = document.querySelectorAll('li');
 // cycle through all of the li tag and attach an event listener to each one of them
 items.forEach(item => {
    item.addEventListener('click', e => {
     // console.log('Item clicked');
-    // console.log(e);
-    // console.log(e.target);
     // change style of item when clicked
-    e.target.style.textDecoration = 'line-through';
+    // e.target.style.textDecoration = 'line-through';
+    e.target.remove(); // will remove the item
 
    }); 
 });
