f12f31e35e8c79d51e70147094a674444f144d76
Jose Carlos <josealarconchacon@gmail.com>
2021-06-16 20:50:44 -0400

    math objects

---
+++ js/object_literal/math_object.js
@@ -0,0 +1,17 @@
+// Math Object
+
+console.log(Math);
+console.log(Math.PI);
+
+
+const area = 7.7;
+console.log(Math.round(area));
+console.log(Math.floor(area));
+const area_ = 5.2;
+console.log(Math.ceil(area_));
+console.log(Math.trunc(area));
+
+
+// random number 1 - 100
+const random = Math.random();
+console.log(Math.random(random * 100));
