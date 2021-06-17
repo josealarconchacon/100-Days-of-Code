e63b38ccd8706c6ee9e8588d430eb64f74ced3cc
Jose Carlos <josealarconchacon@gmail.com>
2021-06-16 21:35:30 -0400

    Query the DOM

---
+++ js/dom/QueryDom/main.js
@@ -0,0 +1,12 @@
+// get element by its ID
+const title = document.getElementById('main_title');
+console.log(title);
+
+// get element by the class name
+const err = document.getElementsByClassName('error');
+console.log(err);
+
+// get element by the tag name
+const paras = document.getElementsByTagName('p');
+console.log(paras);
+console.log(paras[0]);
