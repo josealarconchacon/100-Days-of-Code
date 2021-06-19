6ea164322b4018f79a0be452eab233d961a19a02
Jose Carlos <josealarconchacon@gmail.com>
2021-06-19 18:00:37 -0400

    update files

--- js/dom/Get_Set_Attributes.js/main.js
+++ js/dom/Get_Set_Attributes.js/main.js
@@ -1,14 +1,17 @@
 // get a reference of the element
 const link = document.querySelector('a');
-
 // get the attribute
 console.log(link.getAttribute('href'));
-
 //  set attribute
 link.setAttribute('href','https://www.youtube.com/');
 link.innerHTML = "Youtube Site";
 
-
 const err = document.querySelector('p');
 console.log(err.getAttribute('class'))
 err.setAttribute('class','success');
+
+
+// Changing CSS Style
+const title = document.querySelector('h1');
+// title.setAttribute('style','margin: 50px');
+title.style.color = blue;
