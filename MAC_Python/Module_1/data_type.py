acf25ed9244b9e28d34e15b44e5c92aae3f58ef4
Jose Carlos <josealarconchacon@gmail.com>
2021-06-23 14:45:51 -0400

    data type

---
+++ MAC_Python/Module_1/data_type.py
@@ -0,0 +1,23 @@
+
+'''
+Data Type
+Numbers -> Double ->String ->  Boolean 
+'''
+
+# Number
+print(1)
+print(15 + 5)
+
+# Double
+print(12.8)
+print(12.4 + 3.5)
+
+
+# String
+# String is a sequence of characters, either as a literal constant or as some kind of variable.
+print('Hello' + ' ' + 'Class') # concatenation
+
+
+# Boolean: True, False
+print(True)
+print(False)
