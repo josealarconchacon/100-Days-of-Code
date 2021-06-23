222f2dfcc83407ee44bcd499a0fe0cb820a1fa0d
Jose Carlos <josealarconchacon@gmail.com>
2021-06-23 15:06:56 -0400

    variables

---
+++ MAC_Python/Module_2/variables.py
@@ -0,0 +1,20 @@
+'''
+Variables
+    - Variables are container for storing data values
+'''
+
+age = 95
+name = 'Tom'
+print(f'{name} is {age} years old.')
+
+
+# Casting
+# specify the data type of a variable
+s = str(3) # s will be '3'
+
+num = 20
+greet = 'Hello'
+print(type(num))
+print(type(greet))
+
+
