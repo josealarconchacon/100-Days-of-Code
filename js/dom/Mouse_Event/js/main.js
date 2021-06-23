766b06b564d205741c8c0f5287de476a36405e24
Jose Carlos <josealarconchacon@gmail.com>
2021-06-23 16:14:16 -0400

    3 types of event

---
+++ js/dom/Mouse_Event/js/main.js
@@ -0,0 +1,17 @@
+// get a reference
+const copy_text = document.querySelector('.copy-text');
+// addEventListener
+copy_text.addEventListener('copy_text', () => {
+    console.log("Content was copy");
+});
+
+// Mouse move event
+const box = document.querySelector('.box');
+box.addEventListener('mousemove', e => {
+    // console.log(e.offsetX, e.offsetY);
+    box.textContent = `x possition - ${e.offsetX}, y possition = ${e.offsetY}`
+});
+
+document.addEventListener('wheel',e => {
+    console.log(e.pageX, e.pageY)
+});
