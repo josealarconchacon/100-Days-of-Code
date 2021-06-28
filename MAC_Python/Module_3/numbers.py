c1bcc34fb6f694a3ff51ad1af1111370b1b2a1e7
Jose Carlos <josealarconchacon@gmail.com>
2021-06-28 15:36:04 -0400

    python numbers

---
+++ MAC_Python/Module_3/numbers.py
@@ -0,0 +1,20 @@
+import random
+
+'''
+Three numeric types in Python:
+    int
+    float
+    complex
+'''
+x = 1.0
+print(type(x)) #float
+i = 10
+print(type(i)) #int
+j = 5j
+z = 3+5j
+print(type(j)) #Complex
+print(type(z)) #Complex
+
+
+# Random Numbers
+print(random.randint(1,20))
