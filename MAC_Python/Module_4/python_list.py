d50252a6bb32216907df16464415af5d465715bc
Jose Carlos <josealarconchacon@gmail.com>
2021-06-29 14:57:03 -0400

    list

---
+++ MAC_Python/Module_4/python_list.py
@@ -0,0 +1,18 @@
+# Introduction to python list
+
+myList = ['apple', 'banana', 'mango']
+print(myList)
+print(myList[0:2]) # ['apple', 'banana']
+    
+
+number = [3,5,6,8,9]
+print(number[1])
+# get length of list
+print(len(number))
+
+# combine two list
+print(myList + number) # ['apple', 'banana', 'mango', 3, 5, 6, 8, 9]
+
+
+
+
