53e10ea67e5e3bda710edb309616df175bb6e6de
Jose Carlos <josealarconchacon@gmail.com>
2021-06-29 15:12:05 -0400

    list function

--- MAC_Python/Module_4/python_list.py
+++ MAC_Python/Module_4/python_list.py
@@ -1,12 +1,23 @@
 # Introduction to python list
 
 myList = ['apple', 'banana', 'mango']
+n = [12, 43, 2]
+# print(myList[0:2]) # ['apple', 'banana']
+myList[0] = 'Tomato' # replace item
+myList.insert(2,'peach') # insert
+myList.append('watermelon') # append to the end of the list
+
+# extend the list
+myList.extend(n)
+
+myList.remove('mango') # remove element from list
+del myList[0] # del function
+# myList.clear()  # clear list
 print(myList)
-print(myList[0:2]) # ['apple', 'banana']
     
 
 number = [3,5,6,8,9]
-print(number[1])
+print(number[3])
 # get length of list
 print(len(number))
 
