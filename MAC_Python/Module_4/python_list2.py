e841a183010bea3754da92d0395ac75e3f05e664
Jose Carlos <josealarconchacon@gmail.com>
2021-06-30 14:56:26 -0400

    implementing sort function

---
+++ MAC_Python/Module_4/python_list2.py
@@ -0,0 +1,11 @@
+this_list = ['banana','Orange','Kiwi','cherry']
+this_list.sort(key = str.lower)  # sort alphabetically by this first character
+# print(this_list)
+
+list = ['banana','Orange','Kiwi','cherry']
+list.sort()  # print Uppercase first, then lower case
+print(list)
+
+thisList = ['apple','banana','cherry']
+myList = thisList.copy() # myList now will contain all elements of thisList
+# print(myList)
