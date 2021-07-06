148c75ee3447f3d10e4702f83def2b57e0aee91f
Jose Carlos <josealarconchacon@gmail.com>
2021-07-06 11:40:35 -0400

    sort

--- js/array_methods/sort_methods.js
+++ js/array_methods/sort_methods.js
@@ -1,18 +1,34 @@
 // sorting strings in alphabetical order
 const cities = ['New York', 'Los Angeles', 'Chicago', 'Oakland','Portland'];
 cities.sort();
+// cities.reverse(); - will reverse the string
 console.log(cities);
 
 
-// // sorting numbers
-// const scores = [10,34,54,24,43,70]
+// sorting numbers
+const scores = [10,34,54,24,43,70]
+scores.sort();
+console.log(scores);
 
-// // sorting objects
-// const student_scores = [
-//     {name: 'tommy', score: 90},
-//     {name: 'alicia', score: 80},
-//     {name: 'mauricio', score: 85},
-//     {name: 'raul', score: 96},
-//     {name: 'carlo', score: 93}
-// ];
+
+// // sorting objects by the score
+const student_scores = [
+    {name: 'tommy', score: 90},
+    {name: 'alicia', score: 80},
+    {name: 'mauricio', score: 85},
+    {name: 'raul', score: 96},
+    {name: 'carlo', score: 93}
+];
+
+student_scores.sort((n,s) => {
+    if(n.score > s.score) {
+        return - 1;
+    } else if(s.score > n.score) {
+        return 1;
+    } else {
+        return 0;
+    }
+});
+
+console.log(student_scores);
 
