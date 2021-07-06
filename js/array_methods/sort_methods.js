c5ffd712f98ae2028c825052a9f1876a29346e54
Jose Carlos <josealarconchacon@gmail.com>
2021-07-06 11:30:06 -0400

    sorting strings in alphabetical order

---
+++ js/array_methods/sort_methods.js
@@ -0,0 +1,18 @@
+// sorting strings in alphabetical order
+const cities = ['New York', 'Los Angeles', 'Chicago', 'Oakland','Portland'];
+cities.sort();
+console.log(cities);
+
+
+// // sorting numbers
+// const scores = [10,34,54,24,43,70]
+
+// // sorting objects
+// const student_scores = [
+//     {name: 'tommy', score: 90},
+//     {name: 'alicia', score: 80},
+//     {name: 'mauricio', score: 85},
+//     {name: 'raul', score: 96},
+//     {name: 'carlo', score: 93}
+// ];
+
