d510aa596f0db16960b14df496adcdf832b340e0
Jose Carlos <josealarconchacon@gmail.com>
2021-07-06 14:37:39 -0400

    input func

---
+++ MAC_Python/Module_6/input.py
@@ -0,0 +1,8 @@
+name = input('Enter your name: ')
+street = input('Enter your street number: ')
+zipCode = input('Enter zip code: ')
+city = input('Enter city: \n')
+
+print('Printing user information')
+print('---------------------------------')
+print(f'Name: {name}\nStreet: {street}\nZip Code: {zipCode}\nCity: {city}')
