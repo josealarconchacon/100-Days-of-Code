956d9834b6994adf2e128d2a902cebed6b8c7acf
Jose Carlos <josealarconchacon@gmail.com>
2021-07-06 14:49:17 -0400

    input

--- MAC_Python/Module_6/input.py
+++ MAC_Python/Module_6/input.py
@@ -6,3 +6,14 @@ city = input('Enter city: \n')
 print('Printing user information')
 print('---------------------------------')
 print(f'Name: {name}\nStreet: {street}\nZip Code: {zipCode}\nCity: {city}')
+
+
+
+
+number = int(input('Enter roll number: '))
+age = int(input('Enter age: \n'))
+
+print(f'Roll number: {number}, Age: {age}')
+print('------------------------------------')
+print('Type of input')
+print(f'Number: {type(number)}\n Age: {type(age)}')
