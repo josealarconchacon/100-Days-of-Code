cb1b8a5632b699208e7733af30bf4eee7526230b
Jose Carlos <josealarconchacon@gmail.com>
2021-07-06 15:20:39 -0400

    multiple inputs from the user

--- MAC_Python/Module_6/input1.py
+++ MAC_Python/Module_6/input1.py
@@ -2,3 +2,8 @@ num1 = int(input('Enter any integer: '))
 num2 = float(input('Enter a float number: '))
 product = num1 * num2
 print(f'The multiplication of {num1} * {num2} = {product}')
+
+
+# getting multiple inputs from the user in one line
+name, age, grade = input('Enter name, age and grade, ').split()
+print(f'Name: {name}\nAge: {age}\nGrade: {grade}')
