1f8321046a84cda77611da4245737824675ee164
Jose Carlos <josealarconchacon@gmail.com>
2021-07-07 11:18:32 -0400

    tuple

---
+++ MAC_Python/Module_4/tuples.py
@@ -0,0 +1,10 @@
+# Tuple
+my_typle = ('apple', 'banana', 'orange', 'cherry', 'mango','pineaple', 'kiwi')
+
+# access elements of the tuple
+print(my_typle[2])
+print(my_typle[-1])
+# slice
+print(my_typle[2:5]) # ('orange', 'cherry', 'mango')
+
+
