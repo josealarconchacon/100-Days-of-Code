19a54da5efe78ad4fc5996e74d341ab32b297725
Jose Carlos <josealarconchacon@gmail.com>
2021-07-08 13:19:28 -0400

    digital clock

--- js/date_time/clock/js/app.js
+++ js/date_time/clock/js/app.js
@@ -6,8 +6,17 @@ const tick = () => {
     const now = new Date();
 
     const hours = now.getHours();
-    console.log(hours);
+    const mins = now.getMinutes();
+    const seconds = now.getSeconds();
 
+    // html template
+    const html = `
+    <span>${hours}</span> :
+    <span>${mins}</span> :
+    <span>${seconds}</span> 
+    `;
+    clock.innerHTML = html;
 };
+
 // coll the function every second
 setInterval(tick, 1000);
