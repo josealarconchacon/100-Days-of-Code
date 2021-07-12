16a455edeaa4aca5f9b5eba1eac036240693b1ac
Jose Carlos <josealarconchacon@gmail.com>
2021-07-12 14:58:07 -0400

    range func

---
+++ MAC_Python/Module_7/forLoop.py
@@ -0,0 +1,16 @@
+
+cars = ['Tesla','Lucid','Honda','Toyota']
+for i in cars:
+    print(i, end=', ')
+    if i == 'Lucid':
+        break
+    print(i) # Lucid
+
+
+# range() Function
+for i in range(10 + 1):
+    if i % 2 == 0:
+        print(f'{i} is divisible by 2')
+
+
+
