3ceee05ea5f2c46a7f6e6ac39a21c6405babedbb
Jose Carlos <josealarconchacon@gmail.com>
2021-07-13 10:28:55 -0400

    http request

--- js/async_js/Making_HTTP_Request/js/app.js
+++ js/async_js/Making_HTTP_Request/js/app.js
@@ -2,8 +2,10 @@
 const request = new XMLHttpRequest();
 
 request.addEventListener('readystatechange', () => {
-    if(request.readyState === 4) {
-        console.log(request.responseText);
+    if(request.readyState === 4 && request.status === 200) {
+        console.log(request, request.responseText);
+    } else if(request.readyState === 4) {
+        console.log('Could not fetch data')
     }
 });
 
