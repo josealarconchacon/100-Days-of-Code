d158d959a7639d8c2f648df2a99773fbfcd1a571
Jose Carlos <josealarconchacon@gmail.com>
2021-07-13 14:30:23 -0400

    define a function with two argument

---
+++ MAC_Python1/Module_8/function.py
@@ -0,0 +1,9 @@
+# define a function with one argument
+def myFunc(name):
+    print(f'My name is {name}')
+myFunc('Tom')
+
+# define a function with two argument
+def person_name(f_name, l_name):
+    print(f'My full name is {f_name} {l_name}')
+person_name('Tom', 'De Pino')
