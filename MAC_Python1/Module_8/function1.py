d8a770996986625657c2ba1ccc404282f654d40b
Jose Carlos <josealarconchacon@gmail.com>
2021-07-13 14:46:07 -0400

    Calculate the Area of a Rectangle

--- MAC_Python1/Module_8/function1.py
+++ MAC_Python1/Module_8/function1.py
@@ -5,3 +5,23 @@ def my_cars(car):
 
 cars = ['Tesla', 'Lucid', 'Honda', 'Toyota']
 my_cars(cars)
+
+
+# multiplication of two numbers
+def my_function(x,y):
+    return x * y
+print(my_function(5,4))
+
+
+
+# Calculate the Area of a Rectangle
+def areaOfRect(w, h):
+    # calculate the area
+    area = w * h
+ 
+    print("\n Area of a Rectangle is: %.2f" %area)
+     
+width = float(input('Please Enter the Width: '))
+height = float(input('Please Enter the Height: '))
+
+areaOfRect(width, height)
