2fdfef5e69d3a935f9a033bb6ca7f93fa4cca6ea
Jose Carlos <josealarconchacon@gmail.com>
2021-07-13 10:38:25 -0400

    callback function

--- js/async_js/Making_HTTP_Request/js/app.js
+++ js/async_js/Making_HTTP_Request/js/app.js
@@ -1,13 +1,23 @@
+const get_todo = (callback) => {
+    const request = new XMLHttpRequest();
 
-const request = new XMLHttpRequest();
+    request.addEventListener('readystatechange', () => {
+        if(request.readyState === 4 && request.status === 200) {
+            callback(undefined, request.responseText);
+        } else if(request.readyState === 4) {
+            callback('could not fetch data', undefined);
+        }
+    });
 
-request.addEventListener('readystatechange', () => {
-    if(request.readyState === 4 && request.status === 200) {
-        console.log(request, request.responseText);
-    } else if(request.readyState === 4) {
-        console.log('Could not fetch data')
+    request.open(`GET`, 'https://jsonplaceholder.typicode.com/todos/');
+    request.send();
+};
+
+get_todo((error, data) => {
+    console.log('callback fire');
+    if(error) {
+        console.log(error);
+    } else {
+        console.log(data);
     }
 });
-
-request.open(`GET`, 'https://jsonplaceholder.typicode.com/todos/');
-request.send();
